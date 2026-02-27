d63c4d975fb13887f6998358be2bd78ed652b10b
Abinash965858 <abinashdip01@gmail.com>
2026-02-27 11:45:22 +0530

    commit

---
+++ m46/src/test/java/demo/dummy.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class dummy {
+
+	public static void main(String[] args) {
+		System.out.println("abhinash");
+		System.out.println("hello");
+		System.out.println("41525");
+	}
+}
